6231449bc470420f510a894a35572187eee9bc89
Mau Oliveira <96890754+mauooliveira@users.noreply.github.com>
2022-12-13 18:55:10 +0100

    Update mau_shuffleOutLayers.py

--- mau_shuffleOutLayers.py
+++ mau_shuffleOutLayers.py
@@ -4,20 +4,24 @@
 # last updated: 15.05.22 (DD.MM.YY)
 #--------------------------------------------------
 
-
 # Import the required modules
 import nuke
 
 # Get the selected node
 node = nuke.selectedNode()
+dot = nuke.createNode('Dot')
 
 # Get the list of all layers in the Read node
 channels = node.channels()
-layers = list( set([c.split('.')[0] for c in channels]) )
+layers = list( set([c.split('.')[0] for c in channels]))
+if "rgba" in layers:
+    layers.remove("rgba")
+else:
+    pass
 
 # Create a shuffle node for each layer
 for layer in layers:
   shuffle = nuke.nodes.Shuffle()
-  shuffle.setInput(0, read_node)
-  shuffle.knob("name").setValue(format(layer))
+  shuffle.setInput(0, dot)
+  shuffle.knob("label").setValue(format(layer))
   shuffle["in"].setValue(layer)
